e780ef2ad4d9a7715e32ec7b53c8244ea80d07c9
jason13official <jason13official@gmail.com>
2024-05-13 14:14:24 -0400

    RENAME THE BRANCH, NOT 1.20.1

--- common/src/main/java/com/cursee/instant_horse_tame/CommonClass.java
+++ common/src/main/java/com/cursee/instant_horse_tame/CommonClass.java
@@ -1,7 +1,6 @@
 package com.cursee.instant_horse_tame;
 
 import com.cursee.instant_horse_tame.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
 public class CommonClass {
